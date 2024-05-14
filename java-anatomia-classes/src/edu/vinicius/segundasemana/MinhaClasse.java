beacedeb1ee0b30c42594a17fadcdbe6809a8ece
Vinicius Rosendo <viniciusrosendo17@gmail.com>
2024-05-14 19:44:57 -0300

    métodos-argumentos

---
+++ java-anatomia-classes/src/edu/vinicius/segundasemana/MinhaClasse.java
@@ -0,0 +1,18 @@
+package edu.vinicius.segundasemana;
+public class MinhaClasse {
+    
+public static void main (String [] args) {
+
+    String primeiroNome = "Vinicius";
+    String segundoNome = "Rosendo";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método = " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
